ec4bdd43f2fbd1003a5a3e5bcc447ee1cb757d0b
João Santos <joaopns05@gmail.com>
2019-04-29 17:22:34 +0100

    Implements queue peek operation.

--- redis_queue.py
+++ redis_queue.py
@@ -1,31 +1,60 @@
 import json
+
 from typing import Any, Dict
 
-import redis as redis_py
+from os import getenv
+from redis import Redis
 
 
 class RedisQueue:
-    redis: redis_py.client.Redis
+    redis: Redis
     name: str
 
-    def __init__(self, queue_name: str, host: str = 'localhost', port: int = 6379) -> None:
+    def __init__(self, queue_name: str, host: str = 'localhost', port: int = 6379, db: int = 0) -> None:
+        """Create a redis queue."""
+
         self.name = queue_name
-        self.redis = redis_py.Redis(host=host, port=port)
+        self.redis = Redis(host=host, port=port, db=db)
 
-
-    def enqueue(self, key: str, values: Dict[str, Any]) -> None:
-        list_name = self.name
-        list_values: Dict[str, Any] = {key: values}
+    def enqueue(self, values: Dict[str, Any]) -> None:
+        """Enqueues a value in the queue as a JSON string."""
+        list_values: Dict[str, Any] = values
         json_str: str = json.dumps(list_values)
 
-        self.redis.rpush(list_name, json_str)
+        self.redis.rpush(self.name, json_str)
+
+    def priority_enqueue(self, values: Dict[str, Any]) -> None:
+        """Next dequeue will pop these values."""
+
+        list_values: Dict[str, Any] = values
+        json_str: str = json.dumps(list_values)
+
+        self.redis.lpush(self.name, json_str)
 
     def dequeue(self) -> Dict[str, Any]:
-        list_name = self.name
-        response: str = self.redis.lpop(list_name)
+        """Dequeues a JSON string into a dict."""
+        return json.loads(self.redis.lpop(self.name))
 
-        return json.loads(response)
+    def peek(self) -> Dict[str, Any]:
+        """Returns the element at the head of the queue."""
+        head_element: Dict[str, Any] = json.loads(self.redis.lpop(self.name))
+        json_str: str = json.dumps(head_element)
 
-    def size(self) -> Any:
-        list_name = self.name
-        return self.redis.llen(list_name)
+        self.redis.lpush(self.name, json_str)
+
+        return head_element
+
+    def __len__(self) -> Any:
+        """Returns the current size of the queue."""
+
+        return self.redis.llen(self.name)
+
+
+def initialize_redis_queue(name: str, db: int = 0) -> RedisQueue:
+    """Wrapper function to get redis connection parameters from environment variables."""
+    redis_host = getenv('REDIS_HOST', 'localhost')
+    redis_port = getenv('REDIS_PORT', 6379)
+
+    queue = RedisQueue(queue_name=name, host=redis_host, port=redis_port, db=db)
+
+    return queue
